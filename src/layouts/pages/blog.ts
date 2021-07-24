6b6b62711e9a5bd3dbd455ee0d017bab06e61b08
keiya01 <keiya.s.0210@gmail.com>
2021-07-24 18:48:17 +0900

    fix: fix to use features in blog page

--- src/layouts/pages/blog.ts
+++ src/layouts/pages/blog.ts
@@ -1,6 +1,10 @@
 import { html } from "common-tags";
 
-import { loadScript, loadStyle } from "../../helpers/subResourceHelper";
+import {
+  loadPageScript,
+  loadFeatureScript,
+  loadStyle,
+} from "../../helpers/subResourceHelper";
 import { PageData } from "../../types/eleventy";
 import * as style from "./blog.css";
 
@@ -9,7 +13,7 @@ type Props = {
   content: string;
   page: PageData;
   layout: string;
-  scriptNames: string[] | undefined;
+  features: string[] | undefined;
 };
 
 export const render = ({
@@ -17,26 +21,31 @@ export const render = ({
   content,
   page,
   layout,
-  scriptNames,
-}: Props): string => html`
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <title>${title}</title>
-      <link rel="stylesheet" href="${loadStyle(layout)}" />
-      <script type="module" src="${loadScript(layout)}"></script>
-      ${scriptNames?.map(
-        (name) => `<script type="module" src="${loadScript(name)}"></script>`
-      )}
-    </head>
-    <body class="${style.theme}">
-      <main>
-        <article class="${style.container}">
-          <h1>${title}</h1>
-          <time>${page.date}</time>
-          ${content}
-        </article>
-      </main>
-    </body>
-  </html>
-`;
+  features,
+}: Props): string => {
+  const pageScript = loadPageScript(layout);
+  return html`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>${title}</title>
+        <link rel="stylesheet" href="${loadStyle(layout)}" />
+        ${pageScript &&
+        html`<script type="module" src="${pageScript}"></script>`}
+        ${features?.map(
+          (name) =>
+            `<script type="module" src="${loadFeatureScript(name)}"></script>`
+        )}
+      </head>
+      <body class="${style.theme}">
+        <main>
+          <article class="${style.container}">
+            <h1>${title}</h1>
+            <time>${page.date}</time>
+            ${content}
+          </article>
+        </main>
+      </body>
+    </html>
+  `;
+};
