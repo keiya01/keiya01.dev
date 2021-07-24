440045dff101b72221248d356ab1dea9e77550c9
keiya01 <keiya.s.0210@gmail.com>
2021-07-24 16:17:58 +0900

    feat: add manifest feature

---
+++ build/esbuild/manifest.ts
@@ -0,0 +1,59 @@
+import { Metafile } from "esbuild";
+import { writeFile } from "fs/promises";
+
+type Options = {
+  outputRoot: string;
+};
+
+const AcceptableModuleNames: manifest.AcceptableModuleNames[] = [
+  "layouts",
+  "lib",
+  "pages",
+];
+
+export const generateManifest = async (
+  { outputRoot }: Options,
+  ...metafiles: (Metafile | undefined)[]
+): Promise<void> => {
+  const manifest: manifest.Manifest = {};
+  metafiles.map((metafile) => {
+    if (!metafile) {
+      return;
+    }
+    Object.entries(metafile.outputs || {}).map(
+      ([hashedEntryPoint, { entryPoint }]) => {
+        let formattedEntryPoint = entryPoint;
+
+        const isCSS = hashedEntryPoint.endsWith(".css");
+        if (isCSS) {
+          formattedEntryPoint = hashedEntryPoint.split(".")[0] + ".css";
+        }
+
+        if (!formattedEntryPoint) {
+          return;
+        }
+
+        const splitPath = formattedEntryPoint.split("/");
+        const moduleName = (
+          formattedEntryPoint.startsWith("src/layouts/pages")
+            ? splitPath[2]
+            : splitPath[1]
+        ) as manifest.AcceptableModuleNames;
+        if (
+          !AcceptableModuleNames.includes(
+            moduleName as manifest.AcceptableModuleNames
+          )
+        ) {
+          throw new Error(`${moduleName} is not accepted as entry point`);
+        }
+
+        manifest[moduleName] = {
+          ...(manifest[moduleName] || {}),
+          [formattedEntryPoint]: hashedEntryPoint,
+        };
+      }
+    );
+  });
+
+  await writeFile(`${outputRoot}/manifest.json`, JSON.stringify(manifest));
+};
