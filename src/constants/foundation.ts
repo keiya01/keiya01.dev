15d602530df481145551682f078b945154fe1934
keiya01 <keiya.s.0210@gmail.com>
2021-07-24 16:19:13 +0900

    feat: add loadStyle method

--- src/constants/foundation.ts
+++ src/constants/foundation.ts
@@ -1,3 +1,4 @@
 export const OUTPUT_DIST_DIR = "dist";
 export const ENTRY_POINT_ROOT = "src";
 export const LIB_ENTRY_POINT = `${ENTRY_POINT_ROOT}/lib/pages`;
+export const LAYOUT_ENTRY_POINT = `${OUTPUT_DIST_DIR}/layouts`;
--- src/helpers/subResourceHelper.ts
+++ src/helpers/subResourceHelper.ts
@@ -1,13 +1,26 @@
 import { readFileSync } from "fs";
 import { resolve } from "path";
-import { LIB_ENTRY_POINT, OUTPUT_DIST_DIR } from "../constants/foundation";
+import {
+  LAYOUT_ENTRY_POINT,
+  LIB_ENTRY_POINT,
+  OUTPUT_DIST_DIR,
+} from "../constants/foundation";
 
 const loadJSON = <T extends unknown>(filePath: string): T => {
   return JSON.parse(readFileSync(filePath, "utf8"));
 };
 
-const loadManifest = (): Record<string, string> => {
-  return loadJSON(resolve(__dirname, `../../${OUTPUT_DIST_DIR}/manifest.json`));
+let Manifest: manifest.Manifest | null = null;
+export const loadManifest = (): manifest.Manifest => {
+  if (Manifest) {
+    return Manifest;
+  }
+  const path = resolve(__dirname, `../../${OUTPUT_DIST_DIR}/manifest.json`);
+  Manifest = loadJSON(path);
+  if (!Manifest) {
+    throw new Error(`Could not found manifest.json in ${path}`);
+  }
+  return Manifest;
 };
 
 export const loadScript = (pageName: string): string => {
@@ -15,10 +28,31 @@ export const loadScript = (pageName: string): string => {
 
   const manifest = loadManifest();
 
-  const outputPath = manifest[entryPoint];
+  const outputPath = manifest.lib?.[entryPoint];
 
-  const splitOutputPath = outputPath.split("/");
-  const hashedEntryPointName = splitOutputPath[splitOutputPath.length - 1];
+  if (outputPath) {
+    const splitOutputPath = outputPath.split("/");
+    const hashedEntryPointName = splitOutputPath[splitOutputPath.length - 1];
 
-  return `/lib/${hashedEntryPointName}`;
+    return `/lib/${hashedEntryPointName}`;
+  }
+
+  throw new Error(`Could not found ${pageName} script`);
+};
+
+export const loadStyle = (pageName: string): string => {
+  const entryPoint = `${LAYOUT_ENTRY_POINT}/${pageName}.css`;
+
+  const manifest = loadManifest();
+
+  const outputPath = manifest.layouts?.[entryPoint];
+
+  if (outputPath) {
+    const splitOutputPath = outputPath.split("/");
+    const hashedEntryPointName = splitOutputPath[splitOutputPath.length - 1];
+
+    return `/layouts/${hashedEntryPointName}`;
+  }
+
+  throw new Error(`Could not found ${pageName} style`);
 };
