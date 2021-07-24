2c483415cdc7841f0a857d0ad8d31c517028c2bd
keiya01 <keiya.s.0210@gmail.com>
2021-07-24 16:18:39 +0900

    build: fix 11ty config for css

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,31 @@
+const { readFileSync } = require("fs");
+const { resolve } = require("path");
+
+const loadJSON = (filePath) => {
+  return JSON.parse(readFileSync(filePath, "utf8"));
+};
+
+let _Pages = null;
+const loadPages = () => {
+  if (_Pages) {
+    return _Pages;
+  }
+  _Pages = loadJSON(resolve(__dirname, "./dist/manifest.json")).pages;
+  return _Pages;
+};
+
 module.exports = function (config) {
   config.addPassthroughCopy({ public: "./" });
 
   config.setBrowserSyncConfig({
     files: ["dist/**/*"],
+  });
+
+  const pages = loadPages();
+  Object.values(pages).map((page) => {
+    const filename = page.split("/").slice(-1)[0];
+    const name = filename.split(".")[0];
+    config.addLayoutAlias(name, filename);
   });
 
   return {
