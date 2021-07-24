cfa390b87c0f28987870740c9223bd2887993649
keiya01 <keiya.s.0210@gmail.com>
2021-07-24 18:46:42 +0900

    feat: add features entry point

--- src/constants/foundation.ts
+++ src/constants/foundation.ts
@@ -1,4 +1,5 @@
 export const OUTPUT_DIST_DIR = "dist";
 export const ENTRY_POINT_ROOT = "src";
-export const LIB_ENTRY_POINT = `${ENTRY_POINT_ROOT}/lib/pages`;
+export const LIB_PAGE_ENTRY_POINT = `${ENTRY_POINT_ROOT}/lib/pages`;
+export const LIB_FEATURE_ENTRY_POINT = `${ENTRY_POINT_ROOT}/lib/features`;
 export const LAYOUT_ENTRY_POINT = `${OUTPUT_DIST_DIR}/layouts`;
