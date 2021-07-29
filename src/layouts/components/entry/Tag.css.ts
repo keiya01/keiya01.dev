e4ed3778da88393837f1d716f29be3f6e047e915
keiya01 <keiya.s.0210@gmail.com>
2021-07-29 20:56:37 +0900

    style: refactor Tag.css.ts

--- src/layouts/components/entry/Tag.css.ts
+++ src/layouts/components/entry/Tag.css.ts
@@ -4,7 +4,7 @@ import { vars } from "../../partials/theme.css";
 export const tagItem = style({
   marginLeft: 7,
   borderRadius: 5,
-  backgroundColor: vars.background_tag,
+  background: vars.background_tag,
   fontSize: 14,
   selectors: {
     "&:first-child": {
