6add481b74c07d941ad6430f08b654ee2bf2b213
keiya01 <keiya.s.0210@gmail.com>
2021-08-29 23:31:53 +0900

    refactor: use ogp mod and add style struct

--- build/ogp/src/main.rs
+++ build/ogp/src/main.rs
@@ -1,9 +1,16 @@
-use lib::image::Ogp;
+use lib::image::ogp::{self, Ogp};
 use std::io;
 
 fn main() -> io::Result<()> {
     let text = "これはOGPのタイトルです。ブログのタイトルが入る予定です。Rust言語でブラウザ作ってみた。";
-    let mut ogp = Ogp::new(text, 984., 20., 1.8);
+    let mut ogp = Ogp::new(text, ogp::Style {
+        padding_inline: 20.,
+        line_height: 1.8,
+        font_family: "YuGothic",
+        font_size: 50.,
+        font_style: ogp::FontStyle::Normal,
+        font_weight: ogp::FontWeight::Bold,
+    });
     ogp.generate("./assets/ogp_template.png", "./output.png")?;
     Ok(())
 }
