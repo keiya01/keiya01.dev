9123ce8c029100e1947b29d2c185f60c78451cbb
keiya01 <keiya.s.0210@gmail.com>
2021-08-29 22:33:33 +0900

    feat: add ogp generator

--- build/ogp/src/main.rs
+++ build/ogp/src/main.rs
@@ -1,5 +1,9 @@
-use lib::image::generate_ogp;
+use lib::image::Ogp;
+use std::io;
 
-fn main() {
-    generate_ogp();
+fn main() -> io::Result<()> {
+    let text = "これはOGPのタイトルです。ブログのタイトルが入る予定です。Rust言語でブラウザ作ってみた。";
+    let mut ogp = Ogp::new(text, 984., 20., 1.8);
+    ogp.generate("./assets/ogp_template.png", "./output.png")?;
+    Ok(())
 }
