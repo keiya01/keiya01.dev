b0f250a58357a2d37be7122721bd28e94e798241
keiya01 <keiya.s.0210@gmail.com>
2021-12-19 23:43:20 +0900

    fix: fix svg width

--- blog.keiya01.dev/src/layouts/components/global/Header.ts
+++ blog.keiya01.dev/src/layouts/components/global/Header.ts
@@ -9,7 +9,7 @@ import { LinkIcon } from "./LinkIcon";
 import "../../../lib/components/global/color-scheme-button.css";
 import { ORIGIN } from "../../../constants/origin";
 
-const rssIcon = `<svg aria-label="RSS" height="25" width="25" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>`;
+const rssIcon = `<svg aria-label="RSS" height="25" width="25" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>`;
 
 type Props = {
   title?: string;
