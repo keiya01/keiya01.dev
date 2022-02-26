4c8ab8d2ac0d6eb7b962ff0ef65b3ba4eb25052d
keiya01 <keiya.s.0210@gmail.com>
2022-02-26 22:47:40 +0900

    chore: remove sw

--- blog.keiya01.dev/src/lib/pages/global.ts
+++ blog.keiya01.dev/src/lib/pages/global.ts
@@ -1,4 +1,5 @@
 import "../components/global/color-scheme-button";
 import "../features/webVitals";
-import "../features/sw";
+// TODO: 広告の審査が終わったら有効化する
+// import "../features/sw";
 import "../features/ad";
